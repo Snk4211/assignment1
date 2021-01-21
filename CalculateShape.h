926a2cb7e31e41b3fba76dc83826496c3394ef97
Snk4211 <77761957+Snk4211@users.noreply.github.com>
2021-01-20 21:02:43 -0500

    Create CalculateShape.h

---
+++ CalculateShape.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <math.h>
+class CalculateShapes
+{
+public:
+	double triCal(double x, double y);
+	double trapCal(double b1, double b2, double h);
+	double rectCal(double w, double h, double l);
+
+	
+
+};
